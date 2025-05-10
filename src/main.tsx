ad169dec2b1b34df02a47ef379c426ee44ae9273
Kairat Asylbekov <kairat@mancho.dev>
2025-05-10 23:34:35 +0600

    add font manrope

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import './fonts.css' // Import the font styles first
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
